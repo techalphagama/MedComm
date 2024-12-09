08b9a07517c5d7c3705fb75377a8a7a7bd958dbb
sumitraaho <sumitkumar@raaho.in>
2024-12-09 23:31:43 +0530

    check commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version ("1.9.0") apply false
     id("com.android.library") version "8.1.2" apply false
-//    id("org.jetbrains.kotlin.plugin.serialization") version ("1.9.0") apply true
     kotlin("plugin.serialization") version "1.9.0" apply true
     id("com.google.dagger.hilt.android") version "2.48" apply false
 }
